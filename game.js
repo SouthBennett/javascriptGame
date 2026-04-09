704537024d46069a56064b8bbe407f48a3ae9297
Connor <connorjohnhughes@gmail.com>
2026-04-08 22:30:15 -0700

    Added auto interval counting and one time upgrade

--- game.js
+++ game.js
@@ -8,10 +8,17 @@ let laser = 0;
 let crew = 0;
 let cargo = 0;
 
+let drillBot = 0;
+let betterDrill = 0;
+
+let autobot = 0;
+let drillPurchased =  false;
+
 
 function updateDisplay() {
     document.getElementById("score-display").textContent = 'Minerals Collected: ' + score;
     document.getElementById("rate-display").textContent = 'Harvest Rate +' + pointsPerClick;
+    document.getElementById("auto-display").textContent = 'Auto Mine: +' + autobot + '/sec';
 };
 
 document.getElementById('click-btn').addEventListener("click", function() {
@@ -40,6 +47,20 @@ const upgrades = [
     name: "Bigger Cargo Space",
     cost: 50,
     bonus: pointsPerClick * 5
+  },
+    {
+    id: 4,
+    name: "Drill Bot",
+    cost: 40,
+    bonus: 1,
+    oneTime: true,
+    bought: false
+  },
+  {
+    id: 5,
+    name: "Better Drill Bot",
+    cost: 75,
+    bonus: 2
   }
 ];
 
@@ -50,6 +71,9 @@ function renderUpgrades(){
 
 
   upgrades.forEach(element => {
+    if(element.bought && element.oneTime){
+      return;
+    }
   const div = document.createElement('div');
 
   const button = document.createElement('button');
@@ -78,37 +102,51 @@ function renderUpgrades(){
 
 function buyUpgrade(id) {
   const upgrade = upgrades.find(el => el.id === id);
-    trackUpgrades();
+
   if (score >= upgrade.cost) {
     score -= upgrade.cost;
     showFloatingText("-" + upgrade.cost);
-    pointsPerClick += upgrade.bonus;
-    updateDisplay();
-    checkWin();
-    renderUpgrades();
     
-  }
+
+    
+  
   if(upgrade.id === 1){
+    pointsPerClick += upgrade.bonus;
     laser += 1;
     if(laser > 2){
       upgrade.cost *= 2;
     }
   }else 
   if(upgrade.id === 2){
+    pointsPerClick += upgrade.bonus;
     crew += 1;
     if(crew > 2){
       upgrade.cost *= 2;
     }
   }else 
   if(upgrade.id === 3){
+    pointsPerClick += upgrade.bonus;
     cargo += 1;
     if(cargo > 1){
       upgrade.cost *= 2;
     }
+  }else 
+  if (upgrade.id === 4) {
+    drillBot += 1;
+    autobot += upgrade.bonus;
+    upgrade.bought = true;
+    drillPurchased = true;
+  } else if (upgrade.id === 5) {
+    betterDrill += 1;
+    autobot += upgrade.bonus;
+     upgrade.cost *= 2;
   }
   
-
-  trackUpgrades();
+    updateDisplay();
+    checkWin();
+    renderUpgrades();
+   trackUpgrades();
+  }
 }
 
 function showFloatingText(amount) {
@@ -141,7 +179,18 @@ function trackUpgrades(){
   document.getElementById("laser").textContent = 'Laser: ' + laser;
   document.getElementById("crew").textContent = 'Crew: ' + crew;
   document.getElementById("cargo").textContent = 'Cargo: ' + cargo;
+  document.getElementById("drillBot").textContent = 'Drill Bot: ' + drillBot;
+  document.getElementById("betterDrill").textContent = 'Better Drill Bot: ' + betterDrill;
 
 };
 
+setInterval(function () {
+  score += autobot;
+  updateDisplay();
+  checkWin();
+  renderUpgrades();
+}, 1000);
+
+updateDisplay();
+trackUpgrades();
 renderUpgrades();
