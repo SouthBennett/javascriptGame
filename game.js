5088b38c1f18df66d9ea0faa840c18c99f6be3d0
Connor <connorjohnhughes@gmail.com>
2026-04-08 22:01:18 -0700

    added css and track upgrade system

--- game.js
+++ game.js
@@ -4,10 +4,14 @@ let winner = false;
 let score = 0;
 let pointsPerClick = 1;
 
+let laser = 0;
+let crew = 0;
+let cargo = 0;
+
 
 function updateDisplay() {
-    document.getElementById("score-display").textContent = 'Score: ' + score;
-    document.getElementById("rate-display").textContent = 'Points per click: ' + pointsPerClick;
+    document.getElementById("score-display").textContent = 'Minerals Collected: ' + score;
+    document.getElementById("rate-display").textContent = 'Harvest Rate +' + pointsPerClick;
 };
 
 document.getElementById('click-btn').addEventListener("click", function() {
@@ -21,19 +25,19 @@ document.getElementById('click-btn').addEventListener("click", function() {
 const upgrades = [
   {
     id: 1,
-    name: "double points per click",
+    name: "Space Laser",
     cost: 10,
     bonus: pointsPerClick * 2
   },
   {
     id: 2,
-    name: "triple points per click",
+    name: "Add Crew",
     cost: 20,
     bonus: pointsPerClick * 3
   },
   {
     id: 3,
-    name: "x5 points per click",
+    name: "Bigger Cargo Space",
     cost: 50,
     bonus: pointsPerClick * 5
   }
@@ -62,7 +66,7 @@ function renderUpgrades(){
 
   div.innerHTML = `
     <strong> ${element.name}</strong>
-    Cost: ${element.cost} | +${element.bonus} per click
+    <p>Cost: ${element.cost}</p>  +${element.bonus} added
     
     
   `;
@@ -74,7 +78,7 @@ function renderUpgrades(){
 
 function buyUpgrade(id) {
   const upgrade = upgrades.find(el => el.id === id);
-
+    trackUpgrades();
   if (score >= upgrade.cost) {
     score -= upgrade.cost;
     showFloatingText("-" + upgrade.cost);
@@ -82,8 +86,29 @@ function buyUpgrade(id) {
     updateDisplay();
     checkWin();
     renderUpgrades();
+    
   }
+  if(upgrade.id === 1){
+    laser += 1;
+    if(laser > 2){
+      upgrade.cost *= 2;
+    }
+  }else 
+  if(upgrade.id === 2){
+    crew += 1;
+    if(crew > 2){
+      upgrade.cost *= 2;
+    }
+  }else 
+  if(upgrade.id === 3){
+    cargo += 1;
+    if(cargo > 1){
+      upgrade.cost *= 2;
+    }
+  }
+  
 
+  trackUpgrades();
 }
 
 function showFloatingText(amount) {
@@ -110,5 +135,13 @@ function checkWin() {
     document.body.appendChild(winMessage);
   }
 }
+
+function trackUpgrades(){
+
+  document.getElementById("laser").textContent = 'Laser: ' + laser;
+  document.getElementById("crew").textContent = 'Crew: ' + crew;
+  document.getElementById("cargo").textContent = 'Cargo: ' + cargo;
+
+};
 
 renderUpgrades();
